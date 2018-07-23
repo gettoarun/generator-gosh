1d59f1953c3e6ee4eea444642f886c500057e95b
Arun N. Kumar <arun.kumar2@fadv.com>
2018-07-23 17:50:21 -0400

    Added feature to initialize a project (generating the .projects.yml) file

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,20 +3,52 @@ const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 
+/**
+
+  initializing - Your initialization methods (checking current project state, getting configs, etc)
+  prompting - Where you prompt users for options (where you’d call this.prompt())
+  configuring - Saving configurations and configure the project (creating .editorconfig files and other metadata files)
+  default - If the method name doesn’t match a priority, it will be pushed to this group.
+  writing - Where you write the generator specific files (routes, controllers, etc)
+  conflicts - Where conflicts are handled (used internally)
+  install - Where installations are run (npm, bower)
+  end - Called last, cleanup, say good bye, etc
+
+ */
 module.exports = class extends Generator {
+
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.option('init');
+  }
+
+  // Where you prompt users for options (where you’d call this.prompt())
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the stupendous ${chalk.red('generator-gosh')} generator!`)
+      yosay(`Welcome to ${chalk.red('generator-gosh')}! A simple tool to help your day.`)
     );
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
+        type: 'input',
+        name: 'name',
+        message: `Your project name? ${this.appname}`,
+        default: this.appname
+      },
+      {
+        type: 'choice',
+        name: 'source_type',
+        message: `Select your source control type. Sorry only GIT at this point :(`,
+        choices: ['git'],
+        default: 'git'
+      },
+      {
+        type: 'input',
+        name: 'source_server',
+        message: `Provider your source control server Domain/IP: `,
+      },
     ];
 
     return this.prompt(prompts).then(props => {
@@ -25,14 +57,23 @@ module.exports = class extends Generator {
     });
   }
 
+  // Where you write the generator specific files (routes, controllers, etc)
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.fs.copyTpl(
+      this.templatePath('.projects.yml'),
+      this.destinationPath('.projects.yml'), 
+      {
+        name: this.props.name,
+        version: this.props.version || '1.0.0',
+        source: {
+          type: this.props.source_type,
+          server: this.props.source_server
+        }
+      }
     );
   }
 
   install() {
-    this.installDependencies();
+    //this.installDependencies();
   }
 };
